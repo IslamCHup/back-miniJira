ac90201e2a209e8df279b4ad8adc03bf3fbf90fc
Sterling95 <denxyzaev234@yandex.ru>
2025-12-08 17:44:28 +0300

    implement JWT authentication and authorization middleware

---
+++ internal/middleware/auth.go
@@ -0,0 +1,49 @@
+package middleware
+
+import (
+	"back-minijira-petproject1/internal/auth"
+	"back-minijira-petproject1/internal/repository"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+func AuthMiddleware(repo *repository.UserRepository) gin.HandlerFunc {
+	return func(c *gin.Context){
+		authHeader := c.GetHeader("Authorization")
+
+		if authHeader == ""{
+			c.JSON(http.StatusUnauthorized,gin.H{"error":"missing Authorization header"})
+			c.Abort()
+			return
+		}
+
+		parts := strings.Split(authHeader," ")
+
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized,gin.H{"error":"invalid Authorization format"})
+			c.Abort()
+			return
+		}
+
+		claims, err := auth.ParseToken(parts[1])
+
+		if err != nil {
+			c.JSON(http.StatusUnauthorized,gin.H{"error":"invalid token"})
+			c.Abort()
+			return
+		}
+user, err := repo.GetUserByID(claims.UserID)
+
+if err != nil{
+	c.JSON(http.StatusUnauthorized,gin.H{"error":"user not found"})
+	c.Abort()
+	return
+}
+c.Set("currentUser",user)
+c.Next()
+
+	}
+}
